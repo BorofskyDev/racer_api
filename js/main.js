01d64c635613f5865e84b2e0ee4d37b6b30f1e15
solovyovsdom <joelborofsky@mac.com>
2021-12-04 16:49:46 -0600

    fixed api issue and app now functions

--- js/main.js
+++ js/main.js
@@ -1,62 +1,49 @@
+const api_url =  "https://ergast.com/api/f1/2020/1/driverStandings.json";
 
-let getStandings = document.getElementById("getStandings")
-let userInput = document.getElementById("userInput")
-
-const sendData = () => {
-    axios
-        .post(`https://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
-        .then(response => {
-            console.log(response);
-        });
-};
-
-
-// make a loop 
-// const getStandings = async (season, round) => {
-//     result = await axios.get(`https://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
-//     const response = await fetch (result)
-//     const data = await response.json 
-//     const driverStandings = await data.MRData.StandingsTable.StandingsList[0].DriverStandings
-//     for (racer of driverStandings) {
-//         position.innerHTML = racer.Driver.position 
-//         name.innerHTML = racer.Driver.familyName
-//         nationality.innerHTML = racer.Driver.nationality
-//         sponsor.innerHTML = racer.Constructors[0].name
-//         points.innerHTML = racer.points
-//     }
-// }
-
-getStandings.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let seasonValue = document.getElementById("season").value;
-    let roundValue = document.getElementById('round').value;
-    fetch('https://ergast.com/api/f1/${season}/${round}/driverStandings.json')
-        .then((res) => res.json())
-        .catch(() => displayError())
-    .then((data) => {
-        console.log(data.MRData.StandingsTable.StandingsList[0].DriverStandings)
-        populateRacers(data.MRData.StandingsTable.StandingsList[0].DriverStandings)
-    })
-})
-
-function populateRacers(racers) {
-    request.innerHTML = '';
-    standingsTableBody.innerHTML = ''
-
-    for (const racer of racers) {
-        let newInfo = document.createElement('tr');
-        newInfo.innerHTML = `
-        <td scope="row">${racer.position}</td>
-        <td scope="row">${racer.Driver.givenName} ${racer.Driver.familyName}</td>
-        <td scope="row">${racer.Driver.nationality}</td>
-        <td scope="row">${racer.Constructors[0].name}</td>
-        <td scope="row">${racer.points}</td>`
-
-        standingsTableBody.append(newInfo)
+async function getapi(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
     }
+    populateRacers(data);
 }
 
-function displayError() {
-    standingsTableBody.innerHTML = ''
-    request.innerText = "We were unable to complete your request. Please ensure you have selected a valid year and season and try again."
+function getStandings(event){
+    event.preventDefault()
+    let year = document.getElementById("postSeason").value;
+    let round = document.getElementById("postRound").value;
+    getapi(`https://ergast.com/api/f1/${year}/${round}/driverStandings.json`)
+}
+
+getapi(api_url);
+
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+
+function populateRacers(data) {
+    let tab =
+        `<tr>
+            <th>Position</th>
+            <th>Name</th>
+            <th>Nationality</th>
+            <th>Sponser</th>
+            <th>Points</th>
+        </tr>`;
+    console.log(data.MRData.StandingsTable.StandingsList)
+
+    for (let r of data.MRData.StandingsTable.StandingsLists[0].DriverStandings) {
+        tab += `<tr>
+            <td>${r.position}</td>
+            <td>${r.Driver.givenName} ${r.Driver.familyName}</td>
+            <td>${r.Driver.nationality}</td>
+            <td>${r.Constructors[0].name}</td>
+            <td>${r.points}</td>
+
+        </tr>`
+    }
+
+    document.getElementById("populateRacers").innerHTML = tab;
 }
